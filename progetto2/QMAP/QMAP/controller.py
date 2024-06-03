159271cac8d5153c936be660c199e84dcd40214d
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-06-03 20:21:08 +0200

    iniziamo

---
+++ progetto2/QMAP/QMAP/controller.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    res = HttpResponse(content_type="text/html")
+
+    context = {};
+
+    template = loader.get_template("index.html")
+    page = template.render(context= context , request= request)
+
+    res.write(page)
+
+    return res
