7d574ab392620e156f15574108c7d3e1108b1859
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-07-01 09:04:39 +0200

    OK

---
+++ progetto2/QMAP/static/js/jsHistory.js
@@ -0,0 +1,16 @@
+document.getElementById('backButton').addEventListener('click', function() {
+    window.history.back();
+  });
+
+document.getElementById('backButton2').addEventListener('click', function() {
+  window.history.back();
+});
+
+
+document.getElementById('forwardButton').addEventListener('click', function() {
+  window.history.forward();
+});
+
+document.getElementById('forwardButton2').addEventListener('click', function() {
+  window.history.forward();
+});
